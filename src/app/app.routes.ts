6ef3d257cbb4fc4939f86fa8a5d5082a7fb8a2ca
Saifaly12 <saifaldeinali27@gmail.com>
2026-09-16 23:02:51 +0300

    lazy loadin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,32 +1,96 @@
-import { Us } from './components/us/us';
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
-import { Blog } from './components/blog/blog';
-import { NotFound } from './components/not-found/not-found';
-import { AllArticlesComponent } from './components/blog/all-articles-component/all-articles-component';
-import { EquipmentComponent } from './components/blog/equipment-component/equipment-component';
-import { TechniquesComponent } from './components/blog/techniques-component/techniques-component';
-import { LandscapeComponent } from './components/blog/landscape-component/landscape-component';
-import { PortraitComponent } from './components/blog/portrait-component/portrait-component';
-import { LightingComponent } from './components/blog/lighting-component/lighting-component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: Home, title: 'عدسه | الرئيسيه' },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+
+  {
+    path: 'home',
+    component: Home,
+    title: 'عدسه | الرئيسيه',
+  },
+
   {
     path: 'blog',
-    component: Blog,
+    loadComponent: () => import('./components/blog/blog').then((m) => m.Blog),
     title: 'عدسه | المدونه',
+
     children: [
-      { path: '', redirectTo: 'all', pathMatch: 'full' },
-      { path: 'all', component: AllArticlesComponent, title: 'عدسه | جميع المقالات' },
-      { path: 'equipment', component: EquipmentComponent, title: 'عدسه | معدات' },
-      { path: 'techniques', component: TechniquesComponent, title: 'عدسه | تقنيات' },
-      { path: 'landscape', component: LandscapeComponent, title: 'عدسه | مناظر طبيعية' },
-      { path: 'portrait', component: PortraitComponent, title: 'عدسه | بورتريه' },
-      { path: 'lighting', component: LightingComponent, title: 'عدسه | إضاءة' },
+      {
+        path: '',
+        redirectTo: 'all',
+        pathMatch: 'full',
+      },
+
+      {
+        path: 'all',
+        loadComponent: () =>
+          import('./components/blog/all-articles-component/all-articles-component').then(
+            (m) => m.AllArticlesComponent,
+          ),
+        title: 'عدسه | جميع المقالات',
+      },
+
+      {
+        path: 'equipment',
+        loadComponent: () =>
+          import('./components/blog/equipment-component/equipment-component').then(
+            (m) => m.EquipmentComponent,
+          ),
+        title: 'عدسه | معدات',
+      },
+
+      {
+        path: 'techniques',
+        loadComponent: () =>
+          import('./components/blog/techniques-component/techniques-component').then(
+            (m) => m.TechniquesComponent,
+          ),
+        title: 'عدسه | تقنيات',
+      },
+
+      {
+        path: 'landscape',
+        loadComponent: () =>
+          import('./components/blog/landscape-component/landscape-component').then(
+            (m) => m.LandscapeComponent,
+          ),
+        title: 'عدسه | مناظر طبيعية',
+      },
+
+      {
+        path: 'portrait',
+        loadComponent: () =>
+          import('./components/blog/portrait-component/portrait-component').then(
+            (m) => m.PortraitComponent,
+          ),
+        title: 'عدسه | بورتريه',
+      },
+
+      {
+        path: 'lighting',
+        loadComponent: () =>
+          import('./components/blog/lighting-component/lighting-component').then(
+            (m) => m.LightingComponent,
+          ),
+        title: 'عدسه | إضاءة',
+      },
     ],
   },
-  { path: 'about', component: Us, title: 'عدسه | من نحن' },
-  { path: '**', component: NotFound, title: 'الصفحة غير متوفرة' },
+
+  {
+    path: 'about',
+    loadComponent: () => import('./components/us/us').then((m) => m.Us),
+    title: 'عدسه | من نحن',
+  },
+
+  {
+    path: '**',
+    loadComponent: () => import('./components/not-found/not-found').then((m) => m.NotFound),
+    title: 'الصفحة غير متوفرة',
+  },
 ];
